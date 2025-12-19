45e1f5dc10b5f36f73d8d39dfd6917c8f12a754e
Amelia Johnson <aj764@login2.misha.ycrc.yale.edu>
2025-12-19 18:09:40 -0500

    clean

--- scripts/notebooks/anxiety_metrics/.ipynb_checkpoints/correct-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append('../../../')
-
-from src.classes.pred_loader import PredLoader
-
-preds = PredLoader('large_arena' + '_preds_df.csv')
-
-# preds.get_nan()
-preds.correct()
